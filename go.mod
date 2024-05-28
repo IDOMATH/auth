ac38e9604ad3e484ebd97f1e57a3f2b14da7acd6
IDOMATH <btdothemath@gmail.com>
2024-05-27 22:00:42 -0500

    Initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/IDOMATH/auth
+
+go 1.22.2
+
+require github.com/IDOMATH/session v0.0.0-20240425143228-aeb4b6cf9289
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"github.com/IDOMATH/session/memorystore"
+)
+
+func main() {
+	fmt.Println("hello world")
+	memstore := memorystore.New()
+}
