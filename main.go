2b7a1f6cc745cb3eabf0de55b40a6d7c16bb389c
IDOMATH <btdothemath@gmail.com>
2024-05-28 21:55:35 -0500

    Adding repository

--- main.go
+++ main.go
@@ -3,9 +3,24 @@ package main
 import (
 	"fmt"
 	"github.com/IDOMATH/session/memorystore"
+	"net/http"
 )
+
+type Repository struct {
+	memstore *memorystore.MemoryStore
+}
+
+func NewRepository() *Repository {
+	return &Repository{}
+}
 
 func main() {
 	fmt.Println("hello world")
 	memstore := memorystore.New()
+	repo := NewRepository()
+	repo.memstore = memstore
+}
+
+func handleHome(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Welcome Home"))
 }
