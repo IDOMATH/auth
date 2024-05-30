87ab211906fae38bad5bcd65ce64e6673b99ad7b
IDOMATH <btdothemath@gmail.com>
2024-05-29 20:38:05 -0500

    Adding auth middleware

--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"fmt"
-	"github.com/IDOMATH/session/memorystore"
+	"log"
 	"net/http"
+
+	"github.com/IDOMATH/auth/db"
+	"github.com/IDOMATH/session/memorystore"
 )
 
 type Repository struct {
@@ -15,6 +18,23 @@ func NewRepository() *Repository {
 }
 
 func main() {
+
+	dbHost := "localhost"
+	dbPort := "5432"
+	dbName := "portfolio"
+	dbUser := "postgres"
+	dbPass := "postgres"
+	dbSsl := "disable"
+
+	connectionString := fmt.Sprintf("host=%s port=%s dbname=%s user=%s password=%s sslmode=%s", dbHost, dbPort, dbName, dbUser, dbPass, dbSsl)
+	fmt.Println("Connecting to Postgres")
+	postgresDb, err := db.ConnectSQL(connectionString)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to Postgres")
+
 	fmt.Println("hello world")
 	memstore := memorystore.New()
 	repo := NewRepository()
@@ -23,4 +43,8 @@ func main() {
 
 func handleHome(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Welcome Home"))
+}
+
+func (repo *Repository) auth(next http.HandlerFunc) http.HandlerFunc {
+
 }
